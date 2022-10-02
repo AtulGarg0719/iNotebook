e581fb7ed11d8b825ba63d54c565d3ca4c3f5dda
atul by home laptop <atul60556@gmail.com>
2022-10-02 11:35:50 +0530

    add entry in dtatbase

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,12 +4,12 @@ const User =require('../models/User');
 const { body, validationResult } = require('express-validator');
 
 
-// Route For Create User by Post Method
-router.post('/',[
+// Route For Create User by Post Method  /api/auth
+router.post('/createuser',[
   body('name','Enter Name Please').isLength({ min: 5 }),
   body('email','Enter valid Email').isEmail(),
   body('password').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
   // console.log(req.body);
   // const user = User(req.body);
   // user.save()
@@ -19,12 +19,28 @@ router.post('/',[
   const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    }User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }).then(user => res.json(user)).catch(err =>{console.log(err)
-    res.json({error:'Please Enter unique email',message:err.message})});
+    }
+    // Check where user email already Exits or not 
+    try{
+      let user = await User.findOne({email:req.body.email});
+      console.log(user)
+      if(user){
+        return res.status(400).json({error : "User with this Email already exits"});
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      res.json({"Success":"Your Data is saved successfully"});
+    } 
+    catch (error){
+      console.error(error.message);
+      return res.status(500).send('Some error occur');
+    }
+    
+    // .then(user => res.json(user)).catch(err =>{console.log(err)
+    // res.json({error:'Please Enter unique email',message:err.message})});
     //res.send("Data save successfully!");
   });
 
